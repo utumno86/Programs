e06a45df5a5b8747172ba35e1ac2850281c66672
Michael E. Nash <utumno86@gmail.com>
2015-07-04 12:53:12 -0400

    works for smaller numbers but runtime too long for larger numbers

--- three.rb
+++ three.rb
@@ -3,35 +3,13 @@
 #What is the largest prime factor of the number 600851475143 ?
 #Input: an integer n > 1
 
-#Let A be an array of Boolean values, indexed by integers 2 to n,
-#initially all set to true.
-
- #for i = 2, 3, 4, ..., not exceeding √n:
-  #if A[i] is true:
-    #for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n :
-      #A[j] := false
-
-#Output: all i such that A[i] is true.
-
-values = []
-puts "What number would you like to find the primes of?"
+require 'prime'
+puts "What number would you like to get the prime factors of?"
 num = gets.chomp.to_i
-j = 1
-(1..num).each do |i|
-  values[i] = true
-end
 
-(2..Math.sqrt(num).to_i).each do |i|
-  if values[i+1] == true
-    while (i*j < num)
-      values[i*j] = false
-      j += 1
-    end
-  end
-end
-
-(1..num).each do |i|
-  if values[i] == true
-    puts i
+p = Prime::EratosthenesGenerator.new.take_while {|i| i <= num}
+p.each do |x|
+  if(num%x == 0)
+    puts x
   end
 end
