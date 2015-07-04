3ff8198f45269cc85040fb7c7c851aa35239f102
Michael E. Nash <utumno86@gmail.com>
2015-07-04 13:01:57 -0400

    success

--- three.rb
+++ three.rb
@@ -1,13 +1,8 @@
-#The prime factors of 13195 are 5, 7, 13 and 29.
-
-#What is the largest prime factor of the number 600851475143 ?
-#Input: an integer n > 1
-
 require 'prime'
 puts "What number would you like to get the prime factors of?"
 num = gets.chomp.to_i
 
-p = Prime::EratosthenesGenerator.new.take_while {|i| i <= num}
+p = Prime::EratosthenesGenerator.new.take_while {|i| i <= Math.sqrt(num).to_i}
 p.each do |x|
   if(num%x == 0)
     puts x
