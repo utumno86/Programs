01d83fc70770e83d7826855cd15a5a21ec5b1bf7
Michael E. Nash <utumno86@gmail.com>
2015-08-21 19:56:28 -0400

    nine

--- nine.rb
+++ nine.rb
@@ -1,11 +1,20 @@
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-#
-# a2 + b2 = c2
-# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
-#
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product abc.
-
 puts "What number would you like to find Pythagorean triplets for?"
-answer = gets.chomp.to_i
-puts answer + 1
+number = gets.chomp.to_i
+(1..number).each do |i|
+  (1..(number - i)).each do |j|
+    k = (number - i - j)
+    unless k == 0
+      if(k**2 + j**2 == i**2) && (i+j+k == number)
+        @a = k
+        @b = j
+        @c = i
+      end
+    end
+  end
+end
+
+puts "A: #{@a}  B: #{@b} C: #{@c}"
+puts "A squared + B squared: #{@a**2 + @b**2}"
+puts "C squared: #{@c**2}"
+puts "A + B + C: #{@a+@b+@c}"
+puts "A * B * C: #{@a*@b*@c}"
