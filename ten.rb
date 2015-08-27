69ff98387c4fb8c7b619d5b291fe6d0311485596
Michael E. Nash <utumno86@gmail.com>
2015-08-26 20:57:02 -0400

    whoops

--- ten.rb
+++ ten.rb
@@ -1,5 +1,5 @@
 require 'prime'
-puts "What number would you like to get the prime factors of?"
+puts "What number would you like to get the sum of the primes to?"
 num = gets.chomp.to_i
 
 p = Prime::EratosthenesGenerator.new.take_while {|i| i <= num.to_i}
